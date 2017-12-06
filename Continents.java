e18df02e22501d1936576e802a69593fa1c07f5c
Joel Chester <joel.chester@gmail.com>
2017-12-05 19:41:27 -0500

    Work in progress
    Geo and history...

---
+++ Continents.java
@@ -0,0 +1,60 @@
+package Contient;
+
+public class Continents {
+	public static void main(String[] args) {
+    
+    // Small program design to choose a select a contient
+
+		int continent = 4;
+	    
+		switch (continent) {
+      case 1: System.out.println("North America: Mexico City,    Mexico");
+        break;
+        
+      case 2: System.out.println("South America: Sao Paulo, Brazil");
+        break;
+        
+      case 3: System.out.println("Europe: Mascow, Russia");
+        break;
+        
+      case 4: System.out.println("Africa: Lagos, Nigeria");
+        break;
+        
+      case 5: System.out.println("Asia: Shanghai, China");
+        break;
+        
+      case 6: System.out.println("Australia: Sydney, Australia");
+        break;
+        
+      case 7: System.out.println("Antarctica: McMurdo Station, US");
+        break;
+        
+      case 8: System.out.println("North America: Brooklyn, New York");
+        break;
+        
+      case 9: System.out.println("South America: Georgetown, Guyana");
+        break;
+        
+      case 10: System.out.println("Europe: London, United Kingdom");
+        break;
+        
+      case 11: System.out.println("Africa: Cape Town, South Africa");
+        break;
+        
+      case 12: System.out.println("Asia: Seoul, South Korea");
+        break;
+        
+      case 13: System.out.println("Australia: Melbourne, Australia");
+        break;
+        
+      case 14: System.out.println("North America: Fort Lauderdale, US");
+        break;
+        
+      default: System.out.println("This is an undefine continent! More than likely your in heaven... or hell. Don't forget to take lots of photos. Cheese!");
+        break;
+ 
+    }
+
+	}
+}
+
